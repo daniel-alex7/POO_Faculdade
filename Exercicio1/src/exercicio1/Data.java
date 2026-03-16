154a77a6f982342c8825021cff3319f5c52f6190
DANIEL ROBSON ALEXANDRE SILVA <daniel.silva067@cs.cruzeirodosul.edu.br>
2026-03-16 20:35:29 -0300

    Atualizando POO

--- Exercicio1/src/exercicio1/Data.java
+++ Exercicio1/src/exercicio1/Data.java
@@ -1,14 +1,37 @@
 package exercicio1;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.util.Scanner;
 
-/**
- *
- * @author DANIELROBSONALEXANDR
- */
 public class Data {
+    int dia;
+    int mes;
+    int ano;
     
-}
+    Data(){}
+    
+    Data(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;  
+    }
+    
+    void cadastarDados(){
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite o dia: ");
+        dia = scan.nextInt();
+        
+        System.out.println("Digite o dia: ");
+        mes = scan.nextInt();
+        
+        System.out.println("Digite o dia: ");
+        ano = scan.nextInt();
+    }
+        
+    void ImprimirValor(){
+        System.out.println("A data de hoje é: " + dia +"   mes: "+ mes +"   ano: "+ ano);
+    }
+   
+    }
+    
+
--- Exercicio1/src/exercicio1/Exercicio1.java
+++ Exercicio1/src/exercicio1/Exercicio1.java
@@ -1,23 +1,14 @@
 package exercicio1;
 
-import java.util.Scanner;
-
 public class Exercicio1 {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        Triangulo triangulo1 = new Triangulo(2.0f, 3.0f);
 
-        System.out.println("Digite a base do triângulo:");
-        float base = scan.nextFloat();
-
-        System.out.println("Digite a altura do triângulo:");
-        float altura = scan.nextFloat();
-
-        Triangulo triangulo1 = new Triangulo(base, altura);
-
-        triangulo1.calculaTriangulo();
+        float area_total = 100 * triangulo1.calculaTriangulo();
         triangulo1.ImprimirValor();
-
+        
+        System.out.println("Area Total: "+ area_total);
         
     }
 }
