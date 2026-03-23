a4d79a7bca2a7ea95bd4489126bdc65c94aa323d
DANIEL ROBSON ALEXANDRE SILVA <daniel.silva067@cs.cruzeirodosul.edu.br>
2026-03-23 20:06:11 -0300

    Fim da aula 4

--- Aula04/src/main/java/com/mycompany/aula04/Funcionario.java
+++ Aula04/src/main/java/com/mycompany/aula04/Funcionario.java
@@ -29,13 +29,27 @@ public class Funcionario {
     public float calcularAumento(int tempo){
         return salario + (150 * tempo);
     }
+    
+    
+    void calcularAumento(Funcionario f){
+        if(f.salario > this.salario){
+            this.salario = f.salario;
+        }   
+    }
 
+    
+    //retorno de objeto
+    Funcionario duplicar(){
+        Funcionario f = new Funcionario();
+        f.cracha = this.cracha;
+        f.salario = this.salario;
+        f.cargo = this.cargo;
+        return f;
+    
+    }
+
+    void cacularAumento(Funcionario f2) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
-
-
-          
-    
-    
-    
-
 
