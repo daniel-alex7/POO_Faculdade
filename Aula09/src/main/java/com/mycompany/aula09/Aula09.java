7572049166418451705461d5cb0a513227c184ed
daniel-alex7 <daniel456alexandre@gmail.com>
2026-05-11 19:34:19 -0300

    Aula 9

---
+++ Aula09/src/main/java/com/mycompany/aula09/Aula09.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aula09;
+
+/**
+ *
+ * @author DANIELROBSONALEXANDR
+ */
+public class Aula09 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
